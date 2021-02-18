c8ca1f9e23295d21cb0159f7163ebfa1b615222c
Tony Keiser <keisertony@gmail.com>
2021-02-17 19:34:01 -0800

    Updated Theme
    - Added hover to product grid to show 2nd image
    - Added buttons to add all and remove all items with notifications
    - Added customer banner when logged in

--- assets/js/theme/category.js
+++ assets/js/theme/category.js
@@ -3,12 +3,120 @@ import CatalogPage from './catalog';
 import compareProducts from './global/compare-products';
 import FacetedSearch from './common/faceted-search';
 import { createTranslationDictionary } from '../theme/common/utils/translations-utils';
+import swal from './global/sweet-alert';
 
 export default class Category extends CatalogPage {
     constructor(context) {
         super(context);
         this.validationDictionary = createTranslationDictionary(context);
     }
+    
+    showProductSecondImageOnHover() {
+        const products = $('li.product');
+        
+        products.each((index) => {
+            const product = products[index];
+            const productImages = $(product).find('.card-img-container');
+            
+            if (productImages.length > 1) {
+                $(product).hover(() => {
+                    productImages.each((index) => $(productImages[index]).toggleClass('u-hidden'));
+                });            
+            }
+        });
+    }
+    
+    addAllToCart() {
+        const products = $('li.product');
+        
+        const data = { lineItems: [] };
+        
+        // Build data with products on page
+        products.each((index) => {
+            data.lineItems.push({ 
+                productId: $(products[index]).data('product-id'),
+                quantity: 1
+            })
+        });
+        
+        // Get cart instance
+        $.ajax({
+            "crossDomain": true,
+            "url": window.location.origin + "/api/storefront/carts?include=lineItems.digitalItems.options%2ClineItems.physicalItems.options",
+        }).done((response) => {
+            if (!response[0]) {   
+                // Cart is empty -> create new cart
+                $.ajax({
+                    "crossDomain": true,
+                    "url": window.location.origin + "/api/storefront/carts?include=lineItems.digitalItems.options%2ClineItems.physicalItems.options",
+                    "method": "POST",
+                    "headers": {
+                        "content-type": "application/json"
+                    },
+                    "processData": false,
+                    "data": JSON.stringify(data)
+                }).done((response) => {
+                    // Notify User
+                    swal.fire({
+                        text: 'Items have been added to cart!',
+                        icon: 'success',
+                    }).then(() => {
+                        // Refresh page
+                        window.location.reload();
+                    });
+                });
+            } else {
+                // Add items to existing cart
+                $.ajax({
+                    "crossDomain": true,
+                    "url": window.location.origin + "/api/storefront/carts/" + response[0].id + "/items?include=lineItems.digitalItems.options%2ClineItems.physicalItems.options",
+                    "method": "POST",
+                    "headers": {
+                        "content-type": "application/json"
+                    },
+                    "processData": false,
+                    "data": JSON.stringify(data)
+                }).done((response) => {
+                    // Notify User
+                    swal.fire({
+                        text: 'Items have been added to cart!',
+                        icon: 'success',
+                    }).then(() => {
+                        // Refresh page
+                        window.location.reload();
+                    });
+                });
+            }
+        });
+    }
+    
+    removeAllFromCart() {                
+        // Get cart instance
+        $.ajax({
+            "crossDomain": true,
+            "url": window.location.origin + "/api/storefront/carts?include=lineItems.digitalItems.options%2ClineItems.physicalItems.options",
+        }).done((response) => {
+            if (!response[0]) {
+                return;
+            }
+            
+            // Delete cart
+            $.ajax({
+                "crossDomain": true,
+                "url": window.location.origin + "/api/storefront/carts/" + response[0].id,
+                "method": "DELETE",
+            }).done((response) => {
+                // Notify User
+                swal.fire({
+                    text: 'Cart has been cleared!',
+                    icon: 'success',
+                }).then(() => {
+                    // Refresh page
+                    window.location.reload();
+                });
+            });
+        });  
+    }    
 
     setLiveRegionAttributes($element, roleType, ariaLiveStatus) {
         $element.attr({
@@ -28,6 +136,12 @@ export default class Category extends CatalogPage {
     }
 
     onReady() {
+        this.showProductSecondImageOnHover();
+        
+        $('#add-all-to-cart').on('click', this.addAllToCart);
+        
+        $('#remove-all-items').on('click', this.removeAllFromCart);
+        
         this.arrangeFocusOnSortBy();
 
         $('[data-button-type="add-cart"]').on('click', (e) => this.setLiveRegionAttributes($(e.currentTarget).next(), 'status', 'polite'));
